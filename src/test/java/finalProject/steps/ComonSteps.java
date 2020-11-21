c7be6f3f101f999319c31bd43a361922677b6575
Aleksa-K <aleksandrakalvane@gmail.com>
2020-11-21 19:39:30 +0200

    New commit

--- src/test/java/finalProject/steps/ComonSteps.java
+++ src/test/java/finalProject/steps/ComonSteps.java
@@ -1,19 +1,15 @@
 package finalProject.steps;
 
 
-import com.codeborne.selenide.Condition;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
-import org.assertj.core.api.Assert;
 
-
-import javax.swing.text.Highlighter;
 
 import static com.codeborne.selenide.Condition.disappears;
-import static com.codeborne.selenide.Condition.text;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.addListener;
+
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class ComonSteps {
@@ -23,7 +19,6 @@ public class ComonSteps {
     public void openTheChromeBrowser() {
         open("https://www.1a.lv/");
         getWebDriver().manage().window().maximize();
-        System.out.println("Hello world!");
 
 
     }
