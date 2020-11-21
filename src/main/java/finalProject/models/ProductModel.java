cc1ddbedc1b5ccc7647ee9872e7cc0ff891c7e37
Aleksa-K <aleksandrakalvane@gmail.com>
2020-11-21 23:58:15 +0200

    New commit

--- src/main/java/finalProject/models/ProductModel.java
+++ src/main/java/finalProject/models/ProductModel.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.By;
 public class ProductModel {
 
     private String productName = "Apple Watch Series 6 GPS 40mm Space Gray Aluminum Black Sport Band";
-
+    private By productPrice = By.xpath("//div[@class='product-price-details__block']");
 
 
     public String getProductName() {
         return productName;
     }
 
-
+    public By getProductPrice() {
+        return productPrice;
+    }
 }
--- src/main/java/finalProject/pages/PaymentPage.java
+++ src/main/java/finalProject/pages/PaymentPage.java
@@ -4,22 +4,22 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.impl.Html;
 import org.openqa.selenium.By;
 
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
 import static com.codeborne.selenide.WebDriverRunner.source;
+import static net.bytebuddy.implementation.FixedValue.value;
 
 public class PaymentPage {
 
-
-    private By productPrice = By.xpath("//div[@class='product-price-details__block']");
 
     public void selectPaymentMethod() {
         $(By.id("payment_unused_22")).doubleClick();
         sleep(1000);
     }
 
-    public void priceAssertion() {
-        $(By.xpath("//span[@class='checkout-order-summary-total__price']")).shouldHave(Condition.attribute(String.valueOf(productPrice)));
+    public void priceAssertion(By price) {
+        $(By.xpath("//span[@class='checkout-order-summary-total__price']")).shouldHave((Condition) value(price));
     }
 
     public void validateAPage() {
